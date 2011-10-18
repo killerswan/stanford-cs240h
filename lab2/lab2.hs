e8bf1bc638862e026ab34736e2b461aa94cb1566
Kevin Cantu <me@kevincantu.org>
2011-10-18 00:01:23 -0700

    types for leaves and branches added

--- lab2/lab2.hs
+++ lab2/lab2.hs
@@ -10,15 +10,17 @@
 
 module Main where
 
-import Control.Monad (when)
+--import Control.Monad (when)
 import qualified Data.ByteString.Lazy as B
-import Data.Int
 import Data.List
+{-
+import Data.Int
 import Data.Map (Map)
 import qualified Data.Map as M
 import qualified Data.Text.Lazy as T
 import qualified Data.Text.Lazy.Encoding as TE
 import qualified Data.Text.Lazy.IO as TIO
+-}
 import System.Console.GetOpt
 import System.Environment
 import System.Exit
@@ -28,21 +30,31 @@ import System.IO
 
 
 
-data Pt  = Pt  Integer Integer deriving Show
-data MBR = MBR Pt Pt           deriving Show
-data LHV = LHV Integer         deriving Show
+data Pt  = Pt  Integer Integer deriving (Show, Ord, Eq)
+data MBR = MBR Pt Pt           deriving (Show, Ord, Eq)
+data LHV = LHV Integer         deriving (Show, Ord, Eq)
 
-data BranchNode = BranchNode { mbr :: MBR
-                             , info :: String
-                             , leaf :: Leaf
-                             } deriving Show
+data LeafNode = LeafNode { ln_mbr  :: MBR
+                         , ln_lhv  :: LHV
+                         , ln_info :: String
+                         } deriving (Show, Eq)
 
-data LeafNode = LeafNode { mbr :: MBR
-                         , lhv :: LHV
-                         } deriving Show
+instance Ord LeafNode where
+   -- this way, we can use `insert` to put each node into the list
+   (<=) x y = ln_lhv x <= ln_lhv y
 
-data Branch = [BranchNode] deriving Show
-data Leaf = [LeafNode] deriving Show
+data BranchNode = BranchNode { bn_mbr  :: MBR
+                             , bn_lhv  :: LHV
+                             , bn_leaf :: Branch
+                             } deriving (Show, Eq)
+
+instance Ord BranchNode where
+   (<=) x y = bn_lhv x <= bn_lhv y
+
+data Branch = Leaf [LeafNode]
+            | Branch [BranchNode] deriving (Show, Ord, Eq)
+
+
 
 
 
