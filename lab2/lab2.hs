3c230cc66a1bc31bc0384a144b2bf80c14e6a5d6
Kevin Cantu <me@kevincantu.org>
2011-10-21 03:01:14 -0700

    ok, now i need to write the shuffle functions

--- lab2/lab2.hs
+++ lab2/lab2.hs
@@ -41,17 +41,21 @@ type R = MBR
 type H = LHV
 type Details = String
 
+
+-- Hilbert H Tree
 data Tree = Branch R H [Tree]  -- contains branches and leaves
           | Leaf   R H [Tree]  -- contains only infos
           | Info   R H Details -- 
           deriving (Show, Ord, Eq)
 
 
-leafIsFull :: [Tree] -> Bool
-leafIsFull is = length is >= 3
 
-branchIsFull :: [Tree] -> Bool
-branchIsFull is = length is >= 3
+
+leafIsNotFull :: [Tree] -> Bool
+leafIsNotFull s = length s < 3
+
+branchIsNotFull :: [Tree] -> Bool
+branchIsNotFull s = length s < 3
 
 
 -- big insert function
@@ -63,13 +67,13 @@ insertT :: Tree -- Info
 insertT _ (Info _ _ _) = error "sorry, inserting into an Info makes no sense"
 
 insertT newi (Leaf r h infos) =
-   if leafIsFull infos
-   then Nothing
-   else Just (Leaf r0 h0 infos')
+   if leafIsNotFull infos
+   then Just (Leaf r0 h0 infos')
+   else Nothing
       where
          infos' = insert newi infos
 
-insertT newi (Branch r h trees) =
+insertT newi thisBranch@(Branch r h trees) =
    let
       -- yes, this is a zipper... TODO
       (smallerTs, targetT, biggerTs) =
@@ -78,32 +82,57 @@ insertT newi (Branch r h trees) =
             lessThanH (Info _ h' _) (Leaf   _ h'' _) = h'' < h'
             lessThanH _ _ = error "i am confused: what are you looking for?"
 
-            smallerH' = reverse smallerH
-
-            (smallerH,biggerH) = span (lessThanH newi) trees
+            (smallerH, biggerH) = span (lessThanH newi) trees
          in
-            case biggerH of
-               t:ts -> (smallerH, t, ts)
-               []   -> (reverse . tail $ smallerH', head . reverse $ smallerH', [])
-               where
+            case (reverse smallerH, biggerH) of
+               (sss, t:ts) -> (sss, t, ts)
+               (s:ss, tts) -> (reverse ss, s, tts)
+               (_, _)      -> error "well, this is an empty list..."
 
       siblings = smallerTs -- TODO: think about this
-      siblingsFull s =
+
+      siblingsNotFull s =
          let
-            full (Branch _ _ xs) = branchIsFull xs
-            full (Leaf _ _ xs)   = leafIsFull xs
-            full (Info _ _ _)    = error "an Info can't be full or empty"
+            notFull (Branch _ _ xs) = branchIsNotFull xs
+            notFull (Leaf _ _ xs)   = leafIsNotFull xs
+            notFull (Info _ _ _)    = error "an Info can't be full or empty"
          in
-            elem False . map full $ s
+            elem True . map notFull $ s
+
+      addEmptySibling bs@((Branch _ _ _):bs') = Branch r0 h0 [] : bs
+      addEmptySibling ls@((Leaf   _ _ _):ls') = Leaf   r0 h0 [] : ls
+      addEmptySibling _ = error "sorry, new empty siblings of Info make no sense"
+
    in
       -- recursive call
-      case insertT newi targetT of
-         Just t  -> Just $ Branch r0 h0 (concat [smallerTs, [t], biggerTs])
-         Nothing -> -- DUMMY
-                    Just $ Branch r0 h0 [targetT]
-                     --case siblingsFull siblings
+      case trees of
+         [] -> Nothing
+               -- insertT (Info r0 h0 "wtf") (Branch r0 h0 [])
+
+         _  ->
+            case insertT newi targetT of
+               Just t  -> Just $ Branch r0 h0 (concat [smallerTs, [t], biggerTs])
+                          -- insertT (Info r0 h0 "wtf") (Leaf r0 h0 [])
+
+               Nothing ->
+                  case siblingsNotFull siblings of
+                     True  -> Just $ Branch r0 h0 (shuffleInsert newi siblings)
+                              -- insertT (Info r0 h0 "wtf") (Branch r0 h0 [Leaf r0 h0 []])
+
+                     False ->
+                        case branchIsNotFull trees of
+                           True  -> -- question: how can I guarantee this will give
+                                    --           my target tree free space for the recursive call?
+                                    insertT newi $ Branch r0 h0 (shuffle (addEmptySibling trees))
+
+                                    --  insertT (Info r0 h0 "wtf") (Branch r0 h0 [Leaf r0 h0 [Info r0 h0 "existing 0", Info r0 h0 "existing 1", Info r0 h0 "existing 2"]])
+                           False -> Nothing
 
 
+
+
+shuffleInsert i a = a
+shuffle a  = a
 
 
 
